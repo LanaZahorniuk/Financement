7f5371d0d2348adbe570e37b74aeb0c905b5df25
Lana <zaga.svetul@gmail.com>
2024-04-13 20:52:27 +0200

    test

---
+++ src/main/java/project/financement/FinancementApplication.java
@@ -0,0 +1,13 @@
+package project.financement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancementApplication.class, args);
+	}
+
+}
